3e8ccf08ca69731b58fc3d67f35c2b06d5d789a1
Documenter.jl <documenter@juliadocs.github.io>
2023-10-06 18:52:41 +0000

    build based on cbd3fc5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GlobalOptimization","category":"page"},{"location":"#GlobalOptimization","page":"Home","title":"GlobalOptimization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GlobalOptimization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GlobalOptimization]","category":"page"}]
+}
