7060757d17e2c7dce6cdda225d96e7fc18eeca71
Grant Hecht <granthec@buffalo.edu>
2025-05-05 15:22:46 -0400

    Fianlize updates

--- bench/benchmarking.jl
+++ bench/benchmarking.jl
@@ -368,7 +368,41 @@ function main()
         commit_hash=get_git_commit_hash(),
     )
 
-    return data
+    return data, short_hash
 end
 
-data = main()
+function plot(data, short_hash)
+    # Create directory for plots
+    plot_dir = joinpath(@__DIR__, "data", "plots_$(short_hash)")
+    mkpath(plot_dir)
+
+    # Get unique problem configurations
+    unique_probs = unique(data[:,1:2])
+
+    # Loop over unique problem configs
+    for i in axes(unique_probs, 1)
+        # Get subset
+        pname = unique_probs[i, :ProblemName]
+        ndims = unique_probs[i, :NumDims]
+        data_subset = @subset(data,
+            :ProblemName .== pname,
+            :NumDims .== ndims,
+        )
+
+        # Create figure
+        fig = Figure(;size=(1920,1080))
+        ax = Axis(fig[1,1]; ylabel="Avg. Fitness", yscale=log10)
+        ax.xticks = (axes(data_subset, 1), data_subset[!, :AlgorithmName])
+        ax.xticklabelrotation = 70.0
+
+        # Plot data
+        barplot!(ax, axes(data_subset,1), data_subset[!, :AvgFitness])
+
+        # Save
+        save(joinpath(plot_dir, "$(pname)_$(ndims)dims.pdf"), fig)
+    end
+end
+
+data, shash = main()
+
+plot(data, shash)
