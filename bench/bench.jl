7af44c2d789d7cfe747e833004c0211982505236
Grant Hecht <granthec@buffalo.edu>
2025-05-08 10:35:23 -0400

    Rename bench scripts

--- bench/bench.jl
+++ bench/bench.jl
@@ -3,9 +3,7 @@ using GlobalOptimization
 using Statistics
 using Distributions
 using DataFrames
-using DataFramesMeta
 using JLD2
-using CairoMakie
 
 import BlackBoxOptim as BBO
 
@@ -178,7 +176,7 @@ function algorithm_set()
             "default_de_randto_best_1_bin",
             (p, s, i) -> construct_default_de(p, RandToBest1(), s, i),
         ),
-        ("default_de_unified_bin", (p, s, i) -> construct_de(p, Unified(), s, i)),
+        ("default_de_unified_bin", (p, s, i) -> construct_default_de(p, Unified(), s, i)),
         ("uniform_de_rand_1_bin", (p, s, i) -> construct_de(p, Rand1(), s, i)),
         ("uniform_de_best_1_bin", (p, s, i) -> construct_de(p, Best1(), s, i)),
         (
@@ -391,38 +389,6 @@ function main()
     return data, short_hash
 end
 
-function plot(data, short_hash)
-    # Create directory for plots
-    plot_dir = joinpath(@__DIR__, "data", "plots_$(short_hash)")
-    mkpath(plot_dir)
 
-    # Get unique problem configurations
-    unique_probs = unique(data[:,1:2])
-
-    # Loop over unique problem configs
-    for i in axes(unique_probs, 1)
-        # Get subset
-        pname = unique_probs[i, :ProblemName]
-        ndims = unique_probs[i, :NumDims]
-        data_subset = @subset(data,
-            :ProblemName .== pname,
-            :NumDims .== ndims,
-        )
-
-        # Create figure
-        fig = Figure(;size=(1920,1080))
-        ax = Axis(fig[1,1]; ylabel="Avg. Fitness", yscale=log10)
-        ax.xticks = (axes(data_subset, 1), data_subset[!, :AlgorithmName])
-        ax.xticklabelrotation = 70.0
-
-        # Plot data
-        barplot!(ax, axes(data_subset,1), data_subset[!, :MeanFitness])
-
-        # Save
-        save(joinpath(plot_dir, "$(pname)_$(ndims)dims.pdf"), fig)
-    end
-end
 
 data, shash = main()
-
-#plot(data, shash)
