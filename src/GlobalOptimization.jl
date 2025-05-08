6e804fb61f96a69b4e9b22feac476379e364cc9d
Grant R. Hecht <granthec@buffalo.edu>
2025-05-08 15:57:29 -0400

    Finish initial setup of DE docs (#32)

--- src/GlobalOptimization.jl
+++ src/GlobalOptimization.jl
@@ -49,14 +49,20 @@ include("MBH/MBH.jl")
 
 export ContinuousRectangularSearchSpace
 export OptimizationProblem
+export optimize!
+
 export SerialPSO, ThreadedPSO, PolyesterPSO
 export SerialDE, ThreadedDE, PolyesterDE
+
 export SimpleSelector, RadiusLimitedSelector, RandomSubsetSelector
+
 export Rand1, Rand2, Best1, Best2, CurrentToBest1, CurrentToBest2
 export CurrentToRand1, CurrentToRand2, RandToBest1, RandToBest2, Unified
 export MutationParameters, SelfMutationParameters
+
 export SelfBinomialCrossoverParameters, BinomialCrossoverParameters
+export CovarianceTransformation
+
 #export MBH
-export optimize!
 
 end
