ec8761e6524a993e91c05ea459c38e0eacfac723
GrantHecht <GrantHecht@users.noreply.github.com>
2025-07-08 02:03:35 +0000

    Format .jl files

--- src/utils.jl
+++ src/utils.jl
@@ -36,7 +36,7 @@ Fills the `mat` in-place with the identity matrix.
 function fill_identity!(mat)
     @inbounds for j in axes(mat, 2)
         for i in axes(mat, 1)
-            mat[i,j] = ifelse(i == j, 1.0, 0.0)
+            mat[i, j] = ifelse(i == j, 1.0, 0.0)
         end
     end
     return nothing
